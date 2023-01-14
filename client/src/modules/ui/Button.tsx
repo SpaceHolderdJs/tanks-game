2da7c69f2fe1ad7d04586df5751fed985f770a7a
JuliaShandraDev <julia.shandra.dev@gmail.com>
2023-01-15 00:42:17 +0200

    first changes

---
+++ client/src/modules/ui/Button.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  min-width: 90px;
+  min-height: 30px;
+  background: linear-gradient(360deg, #3f3f3f, #767676);
+  color: white;
+  border-radius: 3px;
+  border: 2px solid #3f3f3f;
+`;
---
+++ client/src/modules/ui/Input.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  min-width: 90px;
+  min-height: 30px;
+  background: linear-gradient(180deg, #0a0a0a, #767676);
+  color: white;
+  border-radius: 3px;
+  border: 2px solid #3f3f3f;
+`;
